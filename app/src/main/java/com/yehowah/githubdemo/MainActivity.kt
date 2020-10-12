44833345ff50c9085b3d0ce9638d37e384a38c77
YehowahHua <chen1124hua@foxmail.com>
2020-10-12 21:52:22 +0800

    develop-01 提交第一次

--- app/src/main/java/com/yehowah/githubdemo/MainActivity.kt
+++ app/src/main/java/com/yehowah/githubdemo/MainActivity.kt
@@ -2,10 +2,19 @@ package com.yehowah.githubdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import javax.security.auth.login.LoginException
 
 class MainActivity : AppCompatActivity() {
+    private  val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+    }
+
+    private fun initView() {
+        Log.i(TAG, "分支提交代码: ")
     }
 }
